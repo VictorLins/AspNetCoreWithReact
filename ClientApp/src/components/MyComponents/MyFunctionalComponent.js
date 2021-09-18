7046f0df0ebcde2731654d183a14d6078ee2ad7f
Victor Campos Lins <victorcampos93@gmail.com>
2021-09-18 13:14:58 +0100

    29-State and Mutability

---
+++ ClientApp/src/components/MyComponents/MyFunctionalComponent.js
@@ -0,0 +1,44 @@
+﻿import React, { useEffect, useState } from 'react';
+
+const MyFunctionalComponent = (props) => {
+
+    /* COMPONENT LIFECYCLE */
+    function componentMount() {
+        //alert("Component rendered");
+    }
+    function componentUnmount() {
+        //alert("Leaving the component");
+    }
+    useEffect(() => {
+        componentMount();
+        return () => {
+            componentUnmount();
+        }
+    }, [])
+
+    /* STATE */
+    const [age, setAge] = useState(20);
+
+    return (
+        <div>
+            <h2>
+                My Functional Component
+
+                {/* PROPS */}
+                < hr />
+                <h4>Name: <b>{props.name ? props.name : "John"}</b></h4>
+
+                {/* STATE */}
+                <hr />
+                <h4>State</h4>
+                <span>
+                    <b>Age: </b>{age}
+                    <button onClick={() => setAge(age + 1)}>+</button>
+                    <button onClick={() => setAge(age - 1)}>-</button>
+                </span>
+            </h2>
+        </div>
+    )
+}
+
+export default MyFunctionalComponent;
