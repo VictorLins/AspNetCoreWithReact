8ba6e7782bd4c4bf6f0105882b9d17f4c4c583b3
Victor Campos Lins <victorcampos93@gmail.com>
2021-09-19 05:02:39 +0100

    31 - Conditional rendering

--- ClientApp/src/components/MyComponents/MyFunctionalComponent.js
+++ ClientApp/src/components/MyComponents/MyFunctionalComponent.js
@@ -19,24 +19,69 @@ const MyFunctionalComponent = (props) => {
     /* STATE */
     const [age, setAge] = useState(20);
 
+    /* EVENT HANDLING */
+    const onChangeAgeInput = (event) => {
+        alert("Age has changed");
+        setAge(parseInt(event.target.value));
+    }
+    const showDetails = (prTelephone) => {
+        alert(`Name: ${props.name ? props.name : 'John'} | Age: ${age} | Telephone: ${prTelephone}`);
+    }
+
+    /* CONDITIONAL RENDERING */
+    let JoanAge = () => {
+        if (age > 25)
+            return (<p>John has more than 25 years</p>)
+        else
+            return (<p>John has 25 years or less</p>)
+    }
+
     return (
         <div>
-            <h2>
-                My Functional Component
+            <h2>My Functional Component</h2>
 
-                {/* PROPS */}
-                < hr />
-                <h4>Name: <b>{props.name ? props.name : "John"}</b></h4>
+            {/* PROPS */}
+            < hr />
+            <h4>Name: <b>{props.name ? props.name : "John"}</b></h4>
 
-                {/* STATE */}
-                <hr />
-                <h4>State</h4>
-                <span>
-                    <b>Age: </b>{age}
-                    <button onClick={() => setAge(age + 1)}>+</button>
-                    <button onClick={() => setAge(age - 1)}>-</button>
-                </span>
-            </h2>
+            {/* STATE */}
+            <hr />
+            <h4>State</h4>
+            <span>
+                <b>Age: </b><input value={age} onChange={onChangeAgeInput} />
+                <button onClick={() => setAge(age + 1)}>+</button>
+                <button onClick={() => setAge(age - 1)}>-</button>
+            </span>
+
+            {/* EVENT HANDLING */}
+            <hr />
+            <h4>Event Handling</h4>
+            <button onClick={showDetails.bind(this, 123456789)}>Show Details</button>
+
+            {/* CONDITIONAL RENDERING */}
+            <hr />
+            <h4>Conditional Rendering</h4>
+
+            {/* EXAMPLE 1 - IF/ELSE */}
+            {JoanAge()}
+
+            {/* EXAMPLE 2 - TERNARY OPERATOR */}
+            <p>{age > 25 ? 'John has more than 25 years' : 'John has 25 years or less'}</p>
+
+            {/* EXAMPLE 3 - SHORT-CIRCUIT OPERATOR */}
+            {age > 25 && <p>John has more than 25 years</p>}
+            {age <= 25 && <p>John has 25 years or less </p>}
+
+            {/* EXAMPLE 4 - IMEDIATELY INVOKED FUNCTION */}
+            {
+                (() => {
+                    switch (age) {
+                        case 25: return <p>John has 25 years</p>;
+                        default: return <p>John has not 25 years</p>;
+                    }
+                })()
+            }
+
         </div>
     )
 }
